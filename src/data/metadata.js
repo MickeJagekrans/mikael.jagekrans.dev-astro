bb29ac8168d6004bf2db5307b303056750aaa3de
MickeJagekrans <micke.jagekrans@gmail.com>
2024-12-05 13:16:19 +0100

    tweak design and add data as js files

---
+++ src/data/metadata.js
@@ -0,0 +1,10 @@
+const metadata = {
+  title: 'Mikael Jagekrans | Polyglot Developer | DevOps Engineer',
+  description: 'Mikael Jagekrans is a polyglot developer and DevOps engineer. This is his CV website.',
+  keywords: 'Mikael Jagekrans, Developer, DevOps, Engineer, Consultant, Freelancer, Back-end, Front-end, Full-stack, CV, Resume',
+  author: 'Mikael Jagekrans',
+  url: 'https://mikael.jagekrans.dev',
+  // image: '/images/profile-original.jpeg',
+};
+
+export default metadata;
